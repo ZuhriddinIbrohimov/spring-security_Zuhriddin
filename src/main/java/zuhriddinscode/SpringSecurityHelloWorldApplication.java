def9a516dcdf52b56e72152f634fc1fe37c85987
ZuhriddinIbrohimov <ibrohimovzuhriddin310@gmail.com>
2024-10-22 09:30:33 +0500

    First Attempt

---
+++ src/main/java/zuhriddinscode/SpringSecurityHelloWorldApplication.java
@@ -0,0 +1,12 @@
+package zuhriddinscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityHelloWorldApplication.class, args);
+	}
+}
---
+++ src/test/java/zuhriddinscode/SpringSecurityHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package zuhriddinscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
